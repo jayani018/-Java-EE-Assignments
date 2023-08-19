4d42b3c84173ecbb3379ee051d79221873d46b90
jayani018 <jayaniarunika11@gmail.com>
2023-08-19 16:25:15 +0530

    Create dto,servlet packages

---
+++ src/lk/ijse/jsons/dto/CustomerDTO.java
@@ -0,0 +1,9 @@
+package lk.ijse.jsons.dto;
+
+/**
+ * @author : Jayani_Arunika  8/19/2023 - 4:23 PM
+ * @since : v0.01.0
+ **/
+
+public class CustomerDTO {
+}
---
+++ src/lk/ijse/jsons/servlet/CustomerServlet.java
@@ -0,0 +1,9 @@
+package lk.ijse.jsons.servlet;
+
+/**
+ * @author : Jayani_Arunika  8/19/2023 - 4:24 PM
+ * @since : v0.01.0
+ **/
+
+public class CustomerServlet {
+}
